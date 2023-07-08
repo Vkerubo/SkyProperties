635685044990a7623c2883f342bd387f6e259a9a
Valentine <kerubovalentine1@gmail.com>
2023-07-09 00:16:58 +0300

    styles

--- src/components/SellersHomePage.js
+++ src/components/SellersHomePage.js
@@ -59,3 +59,32 @@ export const SellersHomePage = ({ seller }) => {
 };
 
 // Styles...
+const styles = {
+  container: {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+    gridGap: "20px",
+    padding: "20px",
+  },
+  propertyContainer: {
+    backgroundColor: "#fff",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+    padding: "20px",
+    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+  },
+  propertyTitle: {
+    fontSize: "24px",
+    fontWeight: "bold",
+    marginBottom: "10px",
+  },
+  propertyImage: {
+    width: "100%",
+    marginBottom: "10px",
+  },
+  propertyInfo: {
+    fontSize: "16px",
+    marginBottom: "5px",
+  },
+};
+
