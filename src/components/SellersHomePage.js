1cecf78cee93a3b686b8568f4fe3f78e269a1a63
Valentine <kerubovalentine1@gmail.com>
2023-07-09 01:30:08 +0300

    handle update functionality

--- src/components/SellersHomePage.js
+++ src/components/SellersHomePage.js
@@ -4,6 +4,7 @@ import { Styles } from "styled-components";
 
 export const SellersHomePage = ({ seller }) => {
   const [sellerProperties, setSellerProperties] = useState(seller?.properties);
+  const [selectedProperty, setSelectedProperty] = useState(null);
 
   const handleUpdateProperty = async (propertyId, updatedData) => {
     try {
@@ -14,32 +15,55 @@ export const SellersHomePage = ({ seller }) => {
       );
       // Handle the response and update the UI if needed
       console.log("Property updated:", response.data);
+      // Update the property in the UI state
+      setSellerProperties((prevProperties) =>
+        prevProperties.map((property) =>
+          property.id === propertyId ? response.data : property
+        )
+      );
+      // Reset the selected property
+      setSelectedProperty(null);
     } catch (error) {
       console.error("Error updating property:", error);
     }
   };
 
-const handleDeleteProperty = async (propertyId) => {
-  try {
-    // Make an API request to delete the property
-    await axios.delete(`/properties/${propertyId}`);
-    // Remove the deleted property from the UI state
-    setSellerProperties((prevProperties) =>
-      prevProperties.filter((property) => property.id !== propertyId)
-    );
-  } catch (error) {
-    console.error("Error deleting property:", error);
-  }
-};
+  const handleDeleteProperty = async (propertyId) => {
+    try {
+      // Make an API request to delete the property
+      await axios.delete(`/properties/${propertyId}`);
+      // Remove the deleted property from the UI state
+      setSellerProperties((prevProperties) =>
+        prevProperties.filter((property) => property.id !== propertyId)
+      );
+    } catch (error) {
+      console.error("Error deleting property:", error);
+    }
+  };
 
+  const handleUpdate = (propertyId) => {
+    // Find the selected property by its ID
+    const propertyToUpdate = sellerProperties.find(
+      (property) => property.id === propertyId
+    );
+    // Set the selected property in the state
+    setSelectedProperty(propertyToUpdate);
+  };
+
+  const handleUpdateFormSubmit = (event) => {
+    event.preventDefault();
+    // Get the updated data from the form inputs
+    const updatedData = {
+      price: event.target.price.value,
+      bathrooms: event.target.bathrooms.value,
+      bedrooms: event.target.bedrooms.value,
+    };
+    // Call the handleUpdateProperty function with the selected property ID and updated data
+    handleUpdateProperty(selectedProperty.id, updatedData);
+  };
 
   const myProperties = sellerProperties?.map((property) => {
     const { id, title, image, price, bathrooms, bedrooms } = property;
-
-    const handleUpdate = () => {
-      const updatedData = { price: 200000 }; // Example: Update price to 200000
-      handleUpdateProperty(id, updatedData);
-    };
 
     const handleDelete = () => {
       handleDeleteProperty(id);
@@ -52,7 +76,28 @@ const handleDeleteProperty = async (propertyId) => {
         <h3 style={styles.propertyInfo}>Price: {price}</h3>
         <h4 style={styles.propertyInfo}>Bathrooms: {bathrooms}</h4>
         <h4 style={styles.propertyInfo}>Bedrooms: {bedrooms}</h4>
-        <button onClick={handleUpdate}>Update</button>
+        {selectedProperty && selectedProperty.id === id ? (
+          <form onSubmit={handleUpdateFormSubmit}>
+            <input
+              type="number"
+              name="price"
+              defaultValue={selectedProperty.price}
+            />
+            <input
+              type="number"
+              name="bathrooms"
+              defaultValue={selectedProperty.bathrooms}
+            />
+            <input
+              type="number"
+              name="bedrooms"
+              defaultValue={selectedProperty.bedrooms}
+            />
+            <button type="submit">Save</button>
+          </form>
+        ) : (
+          <button onClick={() => handleUpdate(id)}>Update</button>
+        )}
         <button onClick={handleDelete}>Delete</button>
       </div>
     );
