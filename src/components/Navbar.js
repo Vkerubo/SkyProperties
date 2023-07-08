4bd355b5f2d02448b65532ad50b777b452ee09df
Valentine <kerubovalentine1@gmail.com>
2023-07-08 17:51:47 +0300

    add hoover effect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,64 +1,81 @@
 import { useRef } from "react";
 import { FaHome } from "react-icons/fa";
+import styled from "styled-components";
+
+const Header = styled.header`
+  background-color: #333;
+  color: #fff;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  width: 40px;
+  height: 40px;
+  margin-right: 10px;
+`;
+
+const Title = styled.h3`
+  font-size: 30px;
+  margin: 0;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+`;
+
+const HomeLink = styled.a`
+  color: #fff;
+  text-decoration: none;
+  margin-right: 20px;
+  transition: color 0.3s, font-size 0.3s;
+
+  &:hover {
+    color: yellow;
+    font-size: 28px;
+  }
+`;
+
+const OtherLink = styled.a`
+  color: #fff;
+  text-decoration: none;
+  margin-right: 10px;
+  transition: color 0.3s, font-size 0.3s;
+
+  &:hover {
+    color: yellow;
+    font-size: 28px;
+  }
+`;
 
 function Navbar() {
   const navRef = useRef();
 
   return (
-    <header style={headerStyle}>
-      <div style={logoContainerStyle}>
-        <img
+    <Header>
+      <LogoContainer>
+        <Logo
           src="https://cdn.pixabay.com/photo/2017/04/08/18/46/house-2214100_640.png"
           alt="Company Logo"
-          style={logoStyle}
         />
-        <h3 style={titleStyle}>Sky Properties</h3>
-      </div>
-      <nav ref={navRef}>
-        <a href="/" style={{ ...linkStyle, marginRight: "35px" }}>
-          <FaHome style={{ color: linkStyle.color, fontSize: "24px" }} />
-        </a>
-        <a href="/login" style={linkStyle}>
-          Login
-        </a>
-        <a href="/signup" style={linkStyle}>
-          Sign Up
-        </a>
-      </nav>
-    </header>
+        <Title>Sky Properties</Title>
+      </LogoContainer>
+      <Nav ref={navRef}>
+        <HomeLink href="/">
+          <FaHome />
+        </HomeLink>
+        <OtherLink href="/login">Login</OtherLink>
+        <OtherLink href="/signup">Sign Up</OtherLink>
+      </Nav>
+    </Header>
   );
 }
 
 export default Navbar;
-
-// Inline styles
-const headerStyle = {
-  backgroundColor: "#333",
-  color: "#fff",
-  padding: "10px",
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-};
-
-const logoContainerStyle = {
-  display: "flex",
-  alignItems: "center",
-};
-
-const logoStyle = {
-  width: "40px",
-  height: "40px",
-  marginRight: "10px",
-};
-
-const titleStyle = {
-  fontSize: "30px",
-  margin: "0",
-};
-
-const linkStyle = {
-  color: "#fff",
-  textDecoration: "none",
-  marginRight: "10px",
-};
